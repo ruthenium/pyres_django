09ba796dbf9542b2fab8c4a14a6320ccbfbfc0aa
ruthenium <bubucektop@gmail.com>
2011-08-08 06:47:45 +0400

    A wsgi wrapper view to use with original resweb application.

--- helpers.py
+++ helpers.py
@@ -1,35 +1,23 @@
-# pyres imports:
+# pyres import
 from pyres import ResQ
 
-# django framework imports:
+# django framework import
 from django.conf import settings
 
-class WebContainer(object):
-    '''
-    Simple wrapper over dictinoaries,
-    to pass to django templates.
-    '''
-    # TODO: perhaps it is better to use
-    # a custom template tag to access a dict.
-    def __init__(self, **kwargs):
-        for k, v in kwargs.items():
-            setattr(self, k, v)
+class ClosingIterator(object):
+    def __init__(self, iterator, close_callback):
+        self.iterator = iter(iterator)
+        self.close_callback = close_callback
 
-def redis_size(resq, key):
-    rk = 'resque:'+key
-    key_type = resq.redis.type(rk)
-    item = 0
-    if key_type == 'list':
-        item = resq.redis.llen(rk)
-    elif key_type == 'set':
-        item = resq.redis.scard(rk)
-    elif key_type == 'string':
-        item = 1
-    return item
+    def __iter__(self):
+        return self
+
+    def next(self):
+        return self.iterator.next()
+
+    def close(self):
+        self.close_callback()
 
 def get_pyres():
-    '''
-    Connection getter.
-    '''
     return ResQ('%s:%d' % (getattr(settings, 'REDIS_HOST', 'localhost'),
         getattr(settings, 'REDIS_PORT', 6379)))
--- urls.py
+++ urls.py
@@ -1,43 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
-from django.shortcuts import redirect
 
 urlpatterns = patterns('pyres_django.views',
-    url(r'^/$', 'overview', name='resweb-index'),
-
-    url(r'^working/$', 'working', name='resweb-working'),
-
-    url(r'^queues/$', 'queues', name='resweb-queues'),
-    url(r'^queues/(?P<queue_id>\w.+)/$', 'queue',
-        name='resweb-queue'),
-    url(r'^queues_delete/(?P<queue_id>\w.+)/$', 'delete_queue',
-        name='resweb-queue-delete'),
-    # Note: pyres currently doesn't support * for queues.
-
-    url(r'^failed/$', 'failed', name='resweb-failed'),
-    url(r'^failed/retry/$', 'failed_job', {'retry':True},
-        name='resweb-failed-retry'),
-    url(r'^failed/delete/$', 'failed_job', name='resweb-failed-delete'),
-
-    url(r'^failed/delete_all/$', 'delete_all_failed',
-        name='resweb-delete-all-failed'),
-    url(r'^failed/retry_all/$', 'retry_failed',
-        name='resweb-retry-all-failed'),
-
-    url(r'^workers/$', 'workers', name='resweb-workers'),
-    url(r'^workers/(?P<worker_id>\w.+)/$', 'worker',
-        name='resweb-worker'),
-
-
-    #url(r'^stats/$', 'stats', name='resweb-stats'),
-    url(r'^stats/$', lambda r: redirect('resweb-stats-key',
-        key='resque'), name='resweb-stats'),
-    url(r'^stats/(?P<key>\w+)/$', 'stats', name='resweb-stats-key'),
-
-    url(r'^stat/(?P<stat_id>\w.+)/$', 'stat', name='resweb-stat'),
-
-
-    url(r'^delayed/$', 'delayed', name='resweb-delayed'),
-
-    url(r'^delayed/(?P<timestamp>\w.+)/$', 'delayed_timestamp',
-        name='resweb-delayed-timestamp'),
+    url(r'^(.+)?$', 'resweb_view'),
 )
